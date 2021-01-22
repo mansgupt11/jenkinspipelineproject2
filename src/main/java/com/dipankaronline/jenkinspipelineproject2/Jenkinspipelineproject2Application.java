56f284a1ed73c2d5d3b0422316c6d24aa296f575
dipankaronline <you@example.com>
2021-01-22 12:42:10 +0530

    added skeleton files

---
+++ src/main/java/com/dipankaronline/jenkinspipelineproject2/Jenkinspipelineproject2Application.java
@@ -0,0 +1,13 @@
+package com.dipankaronline.jenkinspipelineproject2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jenkinspipelineproject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jenkinspipelineproject2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/dipankaronline/jenkinspipelineproject2/Jenkinspipelineproject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dipankaronline.jenkinspipelineproject2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jenkinspipelineproject2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
